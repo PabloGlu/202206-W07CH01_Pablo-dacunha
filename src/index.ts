54eb3f7aa0ad308e19677394359b7fcae7f75e07
Pablo da Cunha <pablodacun@gmail.com>
2022-06-20 18:03:19 +0200

    Starting calculator

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+// import fs from 'fs';
+import fs from 'fs/promises';
+import http from 'http';
+import url from 'url';
+import { iInquirerAnswers, setup } from './setup.js';
+
+const PORT = process.env.PORT || 3204;
+
+// const buffer = fs.readFileSync('./data/data.txt', { encoding: 'utf-8' });
+// console.log(buffer);
+
+const answers: iInquirerAnswers = await setup();
+
+export const server = http.createServer(async (req, res) => {
+    const path = url.parse(req.url as string).path;
+    let dataFile: string = `./data/${path}.txt`;
+
+    try {
+        const data = await fs.readFile(dataFile, { encoding: 'utf-8' });
+        console.log(data);
+        const template = `<h1>Hola, soy ${answers.name}</h1><p>${data}</p>`;
+        res.end(template);
+    } catch (err) {
+        res.end('Error de lectura');
+        server.emit('error', err);
+    }
+
+    /*     const path = url.parse(req.url as string).path;
+    let dataFile: string = `./data/${path}.txt`;
+    fs.readFile(dataFile, { encoding: 'utf-8' })
+        .then((data) => {
+            console.log(data);
+            const template = `<h1>Hola Mundo</h1><p>${data}</p>`;
+            res.end(template);
+        })
+        .catch((err: Error) => {
+            res.end('Error de lectura');
+            console.error(err.message);
+        }); */
+
+    /* fs.readFile(dataFile, { encoding: 'utf-8' }, (err, data) => {
+        if (err) {
+            res.end('Error de lectura');
+            console.log(err.message);
+            return;
+        }
+        console.log(data);
+        const template = `<h1>Hola Mundo</h1><p>${data}</p>`;
+        res.end(template);
+    }); */
+});
+server.listen(PORT);
+console.log(`Server de ${answers.name}
+listening from ${answers.country} in port ${PORT}`);
+
+server.on('error', (err) => {
+    console.error((err as Error).message);
+});
